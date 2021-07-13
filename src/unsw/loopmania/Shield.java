54d64256f1114d2299f298b15a8fa2b88b7777fa
Dongzhu Huang <dong.2000@hotmail.com>
2021-07-13 15:21:39 +1000

    WIP: shield

--- src/unsw/loopmania/Shield.java
+++ src/unsw/loopmania/Shield.java
@@ -3,7 +3,9 @@ package unsw.loopmania;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public class Shield extends ProtectiveGear {
-	private final double critNegationChance = .6;
+	private final double CRIT_NEGATION_CHANCE = .6;
+	private final double ALL_NEGATION_CHANCE = .3;
+
 	public Shield(SimpleIntegerProperty x, SimpleIntegerProperty y, Character wearer) {
 		super(x, y, wearer);
 	}
@@ -19,9 +21,13 @@ public class Shield extends ProtectiveGear {
 		return damage;
 	}
 
-	private boolean negateCrit() {
-		return Math.random() < critNegationChance;
+	private boolean negateAllDamage() {
+		return Math.random() < ALL_NEGATION_CHANCE;
 	}
+
+	// private boolean negateCrit() {
+	// 	return Math.random() < CRIT_NEGATION_CHANCE;
+	// }
 
 	private boolean isVampireCrit(BasicEnemy attacker, boolean isCritical) {
 		return attacker.getClass().equals(Vampire.class) && isCritical;
