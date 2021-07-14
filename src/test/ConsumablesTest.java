948661c912cc959adbb47a6f92218d657d59c0fd
Michael Agius <m.agius@student.unsw.edu.au>
2021-07-14 16:23:11 +1000

    Added test for The One Ring

--- src/test/ConsumablesTest.java
+++ src/test/ConsumablesTest.java
@@ -14,6 +14,7 @@ import unsw.loopmania.LoopManiaWorld;
 import unsw.loopmania.Character;
 import unsw.loopmania.HealthPotion;
 import unsw.loopmania.PathPosition;
+import unsw.loopmania.TheOneRing;
 
 public class ConsumablesTest {
     @Test
@@ -39,5 +40,17 @@ public class ConsumablesTest {
         assertTrue(character.getHp() == 50);
         world.consumeHealthPotion();
         assertTrue(character.getHp() == 80);
+    }
+
+    public void theOneRing() {
+        LoopManiaWorld world = new LoopManiaWorld(8, 14, orderedPath);
+        SimpleIntegerProperty one = new SimpleIntegerProperty(1);
+        TheOneRing theOneRing= new TheOneRing(one, one);
+        PathPosition pos = new PathPosition(0, Arrays.asList(new Pair<>(0, 1), new Pair<>(0, 2), new Pair<>(0, 3))); 
+        Character character = new Character(pos);
+        character.setHp(0)
+        assertTrue(character.getHp() == 0);
+        world.useTheOneRing();
+        assertTrue(character.getHp() == 100);
     }
 }
