10dc05b8b10a6b1224989cd4f18fd0ecfd0ae0c5
Michael Agius <m.agius@student.unsw.edu.au>
2021-07-14 16:28:12 +1000

    Goal class

---
+++ src/unsw/loopmania/Goal.java
@@ -0,0 +1,13 @@
+package unsw.loopmania;
+
+public class Goal {
+    public boolean completed;
+    
+    public boolean getCompletion() {
+        return completed;
+    }
+
+    public void setCompletion(boolean completed) {
+
+    }
+}
