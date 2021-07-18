f103e5058f860b51e97b7c6de8ea952d6854c1af
brydr <67820735+brydr@users.noreply.github.com>
2021-07-18 13:57:03 +1000

    Fixed trap damage test

--- src/test/BuildingTest/TrapDamagesEnemyTest.java
+++ src/test/BuildingTest/TrapDamagesEnemyTest.java
@@ -8,6 +8,7 @@ import org.javatuples.Pair;
 import org.junit.jupiter.api.Test;
 
 import javafx.beans.property.SimpleIntegerProperty;
+import unsw.loopmania.Character;
 import unsw.loopmania.LoopManiaWorld;
 import unsw.loopmania.PathPosition;
 import unsw.loopmania.TrapBuilding;
@@ -16,23 +17,36 @@ import unsw.loopmania.Vampire;
 public class TrapDamagesEnemyTest {
     @Test
     public void trapDamagesEnemyTest() {
+        // Setup world
         var path = Arrays.asList(
             new Pair<>(0, 1),
             new Pair<>(0, 2),
             new Pair<>(0, 3),
-            new Pair<>(0, 4)
+            new Pair<>(0, 4),
+            new Pair<>(0, 5),
+            new Pair<>(0, 6),
+            new Pair<>(0, 7)
         );
+        PathPosition characterPos = new PathPosition(path.indexOf(new Pair<>(0, 7)), path);
+        Character theCharacter = new Character(characterPos);
         LoopManiaWorld world = new LoopManiaWorld(20, 20, path);
+        world.setCharacter(theCharacter);
+
+        // Setup trap
         TrapBuilding trap = new TrapBuilding(new SimpleIntegerProperty(0), new SimpleIntegerProperty(2));
         world.addBuilding(trap);
         
-        // Spawn a vampire on top of a trap
+        // Spawn a vampire near a trap
         Vampire vampire = new Vampire(
             new PathPosition(path.indexOf(new Pair<>(0, 1)), path)
         );
+        world.addEnemies(vampire);
+
         // Assert pre-condition that Vampire has full health
+        // No tick has occured yet so Vampire should have full HP
         assertEquals(vampire.getHp(), Vampire.STARTING_HP);
 
+        // Tick until Vampire steps on trap
         while (!(vampire.getX() == trap.getX() && vampire.getY() == trap.getY())) {
             world.runTickMoves();
         }
