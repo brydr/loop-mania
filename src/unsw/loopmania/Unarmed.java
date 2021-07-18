0b4f298fd4e2506822b241f9fb10460cf2521980
brydr <67820735+brydr@users.noreply.github.com>
2021-07-18 11:27:57 +1000

    Changed Unarmed just so that tests pass

--- src/unsw/loopmania/Unarmed.java
+++ src/unsw/loopmania/Unarmed.java
@@ -1,10 +1,18 @@
 package unsw.loopmania;
 
-public class Unarmed implements WeaponStrategy {
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Unarmed extends Weapon implements WeaponStrategy {
 	private final static int ATTACK_POWER = 2;
 
 	@Override
 	public int getDamage(BasicEnemy enemy) {
 		return Unarmed.ATTACK_POWER;
 	}
+
+	public Unarmed(SimpleIntegerProperty x, SimpleIntegerProperty y) {
+		super(x, y, ATTACK_POWER);
+	}
+
+	
 }
