b8c8fa9839d76dcb4669f6172e0cd0bc2b4e8b18
Benjamin Ryder <ben.ryder@outlook.com>
2021-07-28 13:34:52 +1000

    Refactored allNegationChance to final

--- src/unsw/loopmania/Shield.java
+++ src/unsw/loopmania/Shield.java
@@ -4,16 +4,18 @@ import javafx.beans.property.SimpleIntegerProperty;
 
 public class Shield extends ProtectiveGear {
 	// Critical bite negation is handled in the vampire class
-	private final double allNegationChance = .3d;
+	private final double allNegationChance;
+	private final static double DEFAULT_ALL_NEG_CHANCE = 0.3d;
 	private final static int BUY_PRICE = 200;
 	private final static int SELL_PRICE = 100;
 
 	public Shield(SimpleIntegerProperty x, SimpleIntegerProperty y) {
 		super(x, y, BUY_PRICE, SELL_PRICE);
+		allNegationChance = DEFAULT_ALL_NEG_CHANCE;
 	}
 
 	public Shield(SimpleIntegerProperty x, SimpleIntegerProperty y, double negation_chance) {
-		this(x, y);
+		super(x, y, BUY_PRICE, SELL_PRICE);
 		allNegationChance = negation_chance;
 	}
 
