1d19d6821aa1694948225ffb5594c254b57e7ae8
Michael Agius <m.agius@student.unsw.edu.au>
2021-07-29 18:23:40 +1000

    Added updated end screens

--- src/unsw/loopmania/EndScreenController.java
+++ src/unsw/loopmania/EndScreenController.java
@@ -1,40 +1,50 @@
 package unsw.loopmania;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import java.io.File;
+
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class EndScreenController {
 
-    String msg;
+    private boolean result;
+    private ImageView img = new ImageView();
+    private Image loseImg = new Image((new File("src/images/game_over.gif")).toURI().toString());
+    private Image winImg = new Image((new File("src/images/level_complete.png")).toURI().toString());;
 
     public EndScreenController(boolean result) {
-        if (result) {
-            msg = "Congratulations!! You won!";
-        } else {
-            msg = "You lose :(";
-        }
+        this.result = result;
     }
 
     public Stage runEndScreen() {
         Stage endScreen = new Stage();
         endScreen.setResizable(false);
         endScreen.initModality(Modality.APPLICATION_MODAL);
-        Insets inset = new Insets(20.0f);
         VBox Vbox = new VBox();
-        Vbox.setPadding(inset);
-        Vbox.setAlignment(Pos.CENTER);
-        Vbox.setSpacing(10);
-        Vbox.getChildren().add(new Text(msg));
-
-        Scene endScene = new Scene(Vbox);
+        setEndScreen(endScreen);
+        Vbox.getChildren().add(img);
+        StackPane stack = new StackPane();
+        stack.setStyle("-fx-background-color: black");
+        stack.getChildren().add(Vbox);
+        Scene endScene = new Scene(stack);
         endScreen.setScene(endScene);
         endScreen.show();
         return endScreen;
+    }
+
+    private void setEndScreen(Stage stage) {
+        if (result) {
+            stage.setTitle("Congratulations!");
+            img.setImage(winImg);
+        } else {
+            stage.setTitle("You Lose!");
+            img.setImage(loseImg);
+        }
     }
 
 }
